e1529802e3a398d24631b0bcc4a3cce24b820bad
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-09-24 22:25:22 -0300

    Desenvolvimento das 4 operações da calculadora

--- demo-1/app/calculadora.js
+++ demo-1/app/calculadora.js
@@ -6,3 +6,27 @@
  * Data: 24/09/2017
  * 
  */
+
+'use strict';
+
+module.exports = {
+    //Função responsável por adicionar 2 números:
+    adicionar: function(num1, num2) {
+        return num1 + num2;
+    },
+
+    //Função responsável por subtrair 2 números:
+    subtrair: function(num1, num2) {
+        return num1 - num2;
+    },
+
+    //Função responsável por multiplicar 2 números:
+    multiplicar: function(num1, num2) {
+        return num1 * num2;
+    },
+
+    //Função responsável por dividir 2 números:
+    dividir: function(num1, num2) {
+        return num1 / num2;
+    }
+}
